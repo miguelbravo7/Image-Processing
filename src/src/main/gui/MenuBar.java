a57e53c8938ce2491bf23b0a1e1225f00f6b8ac1
alu0101031538 <alu0101031538@ull.edu.es>
2020-09-30 00:23:37 +0100

    Renamed variables

--- src/src/main/gui/MenuBar.java
+++ src/src/main/gui/MenuBar.java
@@ -26,10 +26,6 @@ public class MenuBar extends JMenuBar {
 		this.program = program;
 		this.frame = program.frame;
 
-		// Set up a panel for the buttons
-		// Create the menu bar.
-
-		// Build the first menu.
 		JMenu menu = new JMenu("Archivo");
 		menu.setMnemonic(KeyEvent.VK_A);
 		menu.getAccessibleContext().setAccessibleDescription("The only menu in this program that has menu items");
@@ -38,44 +34,44 @@ public class MenuBar extends JMenuBar {
 		menu.add(this.simpleMenuItem("Guardar", new ActSaveFile()));
 
 		JMenu filtros = new JMenu("Filtros");
-		JMenu op_punto = new JMenu("Operaciones sobre punto");
+		JMenu pointOperations = new JMenu("Operaciones sobre punto");
 
-		op_punto.add(this.simpleMenuItem("PAL", new PaneAdderAction(() -> Monochrome.renderPal(program.currentImage()), "PAL")));
-		op_punto.add(this.simpleMenuItem("Negativo", new PaneAdderAction(() -> Negative.render(program.currentImage()), "Negativa")));
-		op_punto.add(this.simpleMenuItem("Ecualizacion", new PaneAdderAction(() -> LinealTransform.ecualize(program.currentImage(), program.currentHist()), "Ecualizada")));
-		op_punto.add(this.simpleMenuItem("Correccion de gamma", new ActImageGammaCorrection()));
-		op_punto.add(this.simpleMenuItem("K-means", new ActFilterImageKmeans()));
-		op_punto.add(this.simpleMenuItem("Perfil de imagen", new SimpleMenuAction(__ -> program.crossSectionFlag = true)));
+		pointOperations.add(this.simpleMenuItem("PAL", new PaneAdderAction(() -> Monochrome.renderPal(program.currentImage()), "PAL")));
+		pointOperations.add(this.simpleMenuItem("Negativo", new PaneAdderAction(() -> Negative.render(program.currentImage()), "Negativa")));
+		pointOperations.add(this.simpleMenuItem("Ecualizacion", new PaneAdderAction(() -> LinealTransform.ecualize(program.currentImage(), program.currentHist()), "Ecualizada")));
+		pointOperations.add(this.simpleMenuItem("Correccion de gamma", new ActImageGammaCorrection()));
+		pointOperations.add(this.simpleMenuItem("K-means", new ActFilterImageKmeans()));
+		pointOperations.add(this.simpleMenuItem("Perfil de imagen", new SimpleMenuAction(unused -> program.crossSectionFlag = true)));
 
 		JMenu digitalization = new JMenu("Operaciones sobre punto");
 		digitalization.add(this.simpleMenuItem("Digitalizacion por shifteo", new ActFilterImageDepthShift()));
 		digitalization.add(this.simpleMenuItem("Digitalizacion por region", new ActFilterImageDepthRegion()));
 
-		op_punto.add(digitalization);
+		pointOperations.add(digitalization);
 
-		op_punto.add(this.simpleMenuItem("Cambio brillo-contraste", new ActChangeBC()));
-		op_punto.add(this.simpleMenuItem("Especificacion de histograma", new ActImgHistSpec()));
-		op_punto.add(this.simpleMenuItem("Diferencia de imagen", new ActImgDifference()));
-		op_punto.add(this.simpleMenuItem("Regi�n de inter�s", new SimpleMenuAction(__ -> program.subimageFlag = true)));
-		op_punto.add(this.simpleMenuItem("Transformaci�n lineal", new ActLinealTransform()));
+		pointOperations.add(this.simpleMenuItem("Cambio brillo-contraste", new ActChangeBC()));
+		pointOperations.add(this.simpleMenuItem("Especificacion de histograma", new ActImgHistSpec()));
+		pointOperations.add(this.simpleMenuItem("Diferencia de imagen", new ActImgDifference()));
+		pointOperations.add(this.simpleMenuItem("Region de interes", new SimpleMenuAction(unused -> program.subimageFlag = true)));
+		pointOperations.add(this.simpleMenuItem("Transformacion lineal", new ActLinealTransform()));
 
-		filtros.add(op_punto);
+		filtros.add(pointOperations);
 
-		JMenu op_geom = new JMenu("Operaciones geometricas");
+		JMenu geomOperations = new JMenu("Operaciones geometricas");
 
 		JMenu espejo = new JMenu("Operaciones de espejo");
 		espejo.add(this.simpleMenuItem("Espejo vertical", new PaneAdderAction(() -> Mirror.vertical(program.currentImage()), "Vertical")));
 		espejo.add(this.simpleMenuItem("Espejo horizontal", new PaneAdderAction(() -> Mirror.horizontal(program.currentImage()), "Horizontal")));
 		espejo.add(this.simpleMenuItem("Transpuesta", new PaneAdderAction(() -> Transpose.transpose(program.currentImage()), "Transpuesta")));
 
-		JMenu f_rotations = new JMenu("Rotaciones fixadas");
-		f_rotations.add(this.simpleMenuItem("Rotar 90", new PaneAdderAction(() -> FixedRotation.rotate90(program.currentImage()), "90")));
-		f_rotations.add(this.simpleMenuItem("Rotar 180", new PaneAdderAction(() -> FixedRotation.rotate180(program.currentImage()), "180")));
-		f_rotations.add(this.simpleMenuItem("Rotar 270", new PaneAdderAction(() -> FixedRotation.rotate270(program.currentImage()), "270")));
+		JMenu fRotations = new JMenu("Rotaciones fixadas");
+		fRotations.add(this.simpleMenuItem("Rotar 90", new PaneAdderAction(() -> FixedRotation.rotate90(program.currentImage()), "90")));
+		fRotations.add(this.simpleMenuItem("Rotar 180", new PaneAdderAction(() -> FixedRotation.rotate180(program.currentImage()), "180")));
+		fRotations.add(this.simpleMenuItem("Rotar 270", new PaneAdderAction(() -> FixedRotation.rotate270(program.currentImage()), "270")));
 
-		espejo.add(f_rotations);
+		espejo.add(fRotations);
 
-		op_geom.add(espejo);
+		geomOperations.add(espejo);
 
 		JMenu rotar = new JMenu("Rotar");
 		rotar.add(this.simpleMenuItem("Vecino mas proximo", new ActGeomRotate("Neighbour")));
@@ -83,23 +79,23 @@ public class MenuBar extends JMenuBar {
 		rotar.add(this.simpleMenuItem("Vecino mas proximo Directa", new ActGeomRotateDirect("Neighbour")));
 		rotar.add(this.simpleMenuItem("Bilinear Directa", new ActGeomRotateDirect("Bilinear")));
 
-		op_geom.add(rotar);
+		geomOperations.add(rotar);
 
 		JMenu scale = new JMenu("Cambio de escala");
 		scale.add(this.simpleMenuItem("Vecino mas proximo", new ActGeomScale("Neighbour")));
 		scale.add(this.simpleMenuItem("Bilinear", new ActGeomScale("Bilinear")));
 
-		op_geom.add(scale);
+		geomOperations.add(scale);
 
-		filtros.add(op_geom);
+		filtros.add(geomOperations);
 
 		this.add(filtros);
 
 		JMenu menu3 = new JMenu("Grafos");
-		menu3.add(this.simpleMenuItem("Histograma", new SimpleMenuAction(__ -> program.currentHist().histogram())));
-		menu3.add(this.simpleMenuItem("Histograma acumulado", new SimpleMenuAction(__ -> program.currentHist().histogramAcc())));
-		menu3.add(this.simpleMenuItem("Histograma normalizado", new SimpleMenuAction(__ -> program.currentHist().normHistogram())));
-		menu3.add(this.simpleMenuItem("Histograma normalizado acumulado", new SimpleMenuAction(__ -> program.currentHist().normHistogramAcc())));
+		menu3.add(this.simpleMenuItem("Histograma", new SimpleMenuAction(unused -> program.currentHist().histogram())));
+		menu3.add(this.simpleMenuItem("Histograma acumulado", new SimpleMenuAction(unused -> program.currentHist().histogramAcc())));
+		menu3.add(this.simpleMenuItem("Histograma normalizado", new SimpleMenuAction(unused -> program.currentHist().normHistogram())));
+		menu3.add(this.simpleMenuItem("Histograma normalizado acumulado", new SimpleMenuAction(unused -> program.currentHist().normHistogramAcc())));
 
 		this.add(menu3);
 
