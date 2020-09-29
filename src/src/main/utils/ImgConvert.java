2f8c1032c90e3c2395952c13542fbbd84cb9d4a4
alu0101031538 <alu0101031538@ull.edu.es>
2020-09-29 18:35:13 +0100

    Image conversions are now done on parallel

--- src/src/main/utils/ImgConvert.java
+++ src/src/main/utils/ImgConvert.java
@@ -10,18 +10,16 @@ public class ImgConvert {
 		// Instanciacion de los puntos a partir de los valores de la imagen
 		ArrayList<Pixel> pixels = new ArrayList<Pixel>();
 
-		for (int i = 0; i < image.getHeight(); i++) {
-			for (int j = 0; j < image.getWidth(); j++) {
-				int pixel = image.getRGB(j, i);
+		Utility.imgApply(image, (i, j) -> {
+			int pixel = image.getRGB(j, i);
 
-				int alpha = (pixel >> 24) & 0xff;
-				int red = (pixel >> 16) & 0xff;
-				int green = (pixel >> 8) & 0xff;
-				int blue = (pixel) & 0xff;
+			int alpha = (pixel >> 24) & 0xff;
+			int red = (pixel >> 16) & 0xff;
+			int green = (pixel >> 8) & 0xff;
+			int blue = (pixel) & 0xff;
 
-				pixels.add(new Pixel(new Float[] { (float) alpha, (float) red, (float) green, (float) blue }));
-			}
-		}
+			pixels.add(new Pixel(new Float[] { (float) alpha, (float) red, (float) green, (float) blue }));
+		});
 		return pixels;
 	}
 
@@ -29,29 +27,20 @@ public class ImgConvert {
 		// Instanciacion de los puntos a partir de los valores de la imagen
 		Integer[][] array = new Integer[image.getHeight()][image.getWidth()];
 
-		// Utility.imgApply(image, (i, j) ->
-		for (int i = 0; i < image.getHeight(); i++) {
-			for (int j = 0; j < image.getWidth(); j++) {
-				array[i][j] = image.getRGB(j, i);
-			}
-		}
-		// );
+		Utility.imgApply(image, (i, j) -> array[i][j] = image.getRGB(j, i));
 		return array;
 	}
 
 	public static BufferedImage toBuffImg(List<Pixel> pixelList, int width, int height) {
-
 		BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
 
-		int linea = 0;
-		for (int i = 0; i < height; i++) {
-			for (int j = 0; j < width; j++) {
-				int pixel = (pixelList.get(linea).alpha() << 24) | (pixelList.get(linea).red() << 16)
-						| (pixelList.get(linea).green() << 8) | pixelList.get(linea++).blue(); // pixel
+		Utility.imgApply(img, (i, j) -> {
+			int pixelOffset = i * height + j;
+			int pixel = (pixelList.get(pixelOffset).alpha() << 24) | (pixelList.get(pixelOffset).red() << 16)
+					| (pixelList.get(pixelOffset).green() << 8) | pixelList.get(pixelOffset).blue();
 
-				img.setRGB(j, i, pixel);
-			}
-		}
+			img.setRGB(j, i, pixel);
+		});
 
 		return img;
 	}
