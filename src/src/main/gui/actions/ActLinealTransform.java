16531a472265969c8ff50939a9b9dca822e1e342
alu0101031538 <alu0101031538@ull.edu.es>
2020-10-09 00:34:41 +0100

    Improved LinealTransform interface

--- src/src/main/gui/actions/ActLinealTransform.java
+++ src/src/main/gui/actions/ActLinealTransform.java
@@ -2,74 +2,149 @@ package main.gui.actions;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.Point;
 import java.awt.Container;
 import java.awt.Component;
 import java.awt.FlowLayout;
+import javax.swing.text.NumberFormatter;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerModel;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.event.ChangeEvent;
 
 import main.filters.point.LinealTransform;
 import main.gui.Menu;
+import main.gui.graphs.LineGraph;
 
 public class ActLinealTransform implements ActionListener {
+    List<Point> mappings;
+    JPanel xPanel;
+    JPanel yPanel;
+    LineGraph graph;
+
     public void actionPerformed(ActionEvent evt) {
-        JFrame popup = new JFrame("Diferencia de imagen");
-        popup.setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-        JPanel panelx = new JPanel();
-        JPanel panely = new JPanel();
-        panelx.setLayout(new BoxLayout(panelx, BoxLayout.Y_AXIS));
-        panely.setLayout(new BoxLayout(panely, BoxLayout.Y_AXIS));
-        panelx.add(new JTextField("x:", 6));
-        panely.add(new JTextField("y:", 6));
-        popup.setLayout(new FlowLayout());
+        JFrame popup = new JFrame("Transformacion lineal");
+        xPanel = new JPanel();
+        yPanel = new JPanel();
+        xPanel.setLayout(new BoxLayout(xPanel, BoxLayout.Y_AXIS));
+        yPanel.setLayout(new BoxLayout(yPanel, BoxLayout.Y_AXIS));
+        xPanel.add(new JLabel("Valor original"));
+        xPanel.add(numberSpinner(200));
+        xPanel.add(numberSpinner(210));
+        yPanel.add(new JLabel("Valor objetivo"));
+        yPanel.add(numberSpinner(62));
+        yPanel.add(numberSpinner(55));
 
-        JButton addpoint = new JButton("Anadir punto");
+        JPanel coordPanel = new JPanel();
+        coordPanel.add(xPanel);
+        coordPanel.add(yPanel);
+        popup.add(coordPanel);
 
-        addpoint.addActionListener((ActionEvent e) -> {
-            panelx.add(new JTextField(6));
-            panelx.revalidate();
-            panelx.repaint();
+        JPanel rightPanel = new JPanel();
+        rightPanel.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
 
-            panely.add(new JTextField(6));
-            panely.revalidate();
-            panely.repaint();
+        mappings = iterateOverJTextFields(xPanel, yPanel);
+        graph = new LineGraph(mappings);
+        // Put constraints on different buttons
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        rightPanel.add(graph, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 1;
+        JButton addPoint = new JButton("Anadir punto");
+        addPoint.addActionListener((ActionEvent e) -> {
+            xPanel.add(numberSpinner(0));
+            yPanel.add(numberSpinner(0));
+            mappings.clear();
+            mappings.addAll(iterateOverJTextFields(xPanel, yPanel));
+            graph.repaint();
+            popup.pack();
         });
-        popup.add(addpoint);
-        JPanel panels = new JPanel();
-        panels.add(panelx);
-        panels.add(panely);
+        rightPanel.add(addPoint, gbc);
 
-        JButton okbutton = new JButton("Ok");
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        JButton delPoint = new JButton("Eliminar punto");
+        delPoint.addActionListener((ActionEvent e) -> {
+            if(mappings.size() > 2) {
+                xPanel.remove(mappings.size());
+                yPanel.remove(mappings.size());
+                mappings.remove(0);
+                graph.repaint();
+                popup.pack();
+            }
+        });
+        rightPanel.add(delPoint, gbc);
 
-        okbutton.addActionListener((ActionEvent e) -> {
-            Menu.addToPane(LinealTransform.transform(Menu.currentImage(),
-                    iterateOverJTextFields(panelx, panely)), "Trans. lineal");
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.gridwidth = 2;
+        JButton okButton = new JButton("Ok");
+        okButton.addActionListener((ActionEvent e) -> {
+            Menu.addToPane(LinealTransform.transform(Menu.currentImage(), iterateOverJTextFields(xPanel, yPanel)),
+                    "Trans. lineal");
             popup.dispose();
         });
-        popup.add(okbutton);
-        popup.add(panels);
+        rightPanel.add(okButton, gbc);
 
+        popup.add(rightPanel);
+
+        popup.setLayout(new FlowLayout());
+        popup.setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         popup.pack();
         popup.setVisible(true);
     }
 
-    private ArrayList<Point> iterateOverJTextFields(Container xcontainer, Container ycontainer) {
-        ArrayList<Integer> x = new ArrayList<>();
+    private JSpinner numberSpinner(int value) {
+        SpinnerModel model = new SpinnerNumberModel(value, 0, 255, 1);
+        JSpinner spinner = new JSpinner(model);
+        spinner.setEditor(new JSpinner.NumberEditor(spinner, "#"));
+        spinner.addChangeListener((ChangeEvent c) -> {
+            mappings.clear();
+            mappings.addAll(iterateOverJTextFields(xPanel, yPanel));
+            graph.repaint();
+        });
+        JFormattedTextField textField = ((JSpinner.NumberEditor) spinner.getEditor()).getTextField();
+        ((NumberFormatter) textField.getFormatter()).setAllowsInvalid(false);
+        textField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+                mappings.clear();
+                mappings.addAll(iterateOverJTextFields(xPanel, yPanel));
+                graph.repaint();
+            }
+        });
+        return spinner;
+    }
+
+    private ArrayList<Point> iterateOverJTextFields(Container xContainer, Container yContainer) {
+        ArrayList<Integer> xCoordinates = new ArrayList<>();
         ArrayList<Point> points = new ArrayList<>();
-        for (Component component : xcontainer.getComponents()) {
-            if (component instanceof JTextField) {
-                x.add(Integer.parseInt(((JTextField) component).getText()));
+        for (Component component : xContainer.getComponents()) {
+            if (component instanceof JSpinner) {
+                xCoordinates.add((Integer) ((JSpinner) component).getValue());
             }
         }
-        for (Component component : ycontainer.getComponents()) {
-            if (component instanceof JTextField) {
-                points.add(new Point(x.remove(0), Integer.parseInt(((JTextField) component).getText())));
+        for (Component component : yContainer.getComponents()) {
+            if (component instanceof JSpinner) {
+                points.add(new Point(xCoordinates.remove(0), (Integer) ((JSpinner) component).getValue()));
             }
         }
         return points;
