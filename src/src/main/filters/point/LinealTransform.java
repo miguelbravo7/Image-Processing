efdc354db841c7b6c7d52bb44333f8f96103cecf
alu0101031538 <alu0101031538@ull.edu.es>
2020-10-09 00:33:52 +0100

    Optimized intersection algorithm

--- src/src/main/filters/point/LinealTransform.java
+++ src/src/main/filters/point/LinealTransform.java
@@ -2,6 +2,7 @@ package main.filters.point;
 
 import java.awt.Point;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -9,10 +10,13 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import main.gui.Histogram;
+import main.utils.Pair;
 import main.utils.Utility;
 
 public class LinealTransform {
 	private static final Logger LOGGER = Logger.getLogger(LinealTransform.class.getName());
+	private static final int UPPER_BOUND = 255;
+	private static final int LOWER_BOUND = 0;
 
 	private LinealTransform() {
 		throw new IllegalStateException("Utility class");
@@ -20,9 +24,9 @@ public class LinealTransform {
 
 	public static BufferedImage transform(BufferedImage image, List<Point> points) {
 		BufferedImage img = new BufferedImage(image.getWidth(), image.getHeight(), image.getType());
-
 		Collections.sort(points, new PointCompare());
-		traducePoints(points);
+		List<Point> filledPoints = fillPoints(points);
+
 		for (Point p : points) {
 			LOGGER.log(Level.FINE, "{0}", p);
 		}
@@ -35,9 +39,9 @@ public class LinealTransform {
 			int green = (pixel >> 8) & 0xff;
 			int blue = (pixel) & 0xff;
 
-			int redValue = traduceValue(red, points);
-			int greenValue = traduceValue(green, points);
-			int blueValue = traduceValue(blue, points);
+			int redValue = traduceValue(red, filledPoints);
+			int greenValue = traduceValue(green, filledPoints);
+			int blueValue = traduceValue(blue, filledPoints);
 
 			pixel = (alpha << 24) | (redValue << 16) | (greenValue << 8) | blueValue; // pixel
 
@@ -47,50 +51,62 @@ public class LinealTransform {
 		return img;
 	}
 
-	private static void traducePoints(List<Point> points) {
-		if (points.get(0).x != 0) { // y = ax + b
-			double pendiente = (points.get(1).y - points.get(0).y) / (double) (points.get(1).x - points.get(0).x);
-			int offset = points.get(0).x - points.get(0).y;
-
-			// Looking for slope cutting points
-			if (pendiente == 0) {
-				points.add(0, new Point(0, offset));
-			} else if (pendiente > 0) {
-				int cutPoint = (int) (-offset / pendiente);
-
-				points.add(0, new Point(cutPoint, 0));
-				if (cutPoint > 0)
-					points.add(0, new Point(0, 0));
-			} else {
-				int cutPoint = (int) (255 - offset / pendiente);
-
-				points.add(0, new Point(cutPoint, 255));
-				if (cutPoint > 0)
-					points.add(0, new Point(0, 255));
+	public static List<Point> fillPoints(List<Point> points) {
+		List<Point> pointsCopy = new ArrayList<>(points);
+		if (pointsCopy.get(0).x != LOWER_BOUND) {
+			Pair<Point, Point> res = boundedIntersection(pointsCopy.get(0), pointsCopy.get(1));
+			pointsCopy.add(0, res.getLeft());
+			if (res.getLeft().getX() > LOWER_BOUND) {
+				pointsCopy.add(0, new Point(LOWER_BOUND, LOWER_BOUND));
 			}
 		}
-		int lastPoint = points.size() - 1;
-		if (points.get(lastPoint).x != 255) {
-			double pendiente = (points.get(lastPoint).y - points.get(lastPoint - 1).y)
-					/ (double) (points.get(lastPoint).x - points.get(lastPoint - 1).x);
-			int offset = points.get(lastPoint - 1).x - points.get(lastPoint - 1).y;
-
-			if (pendiente == 0) {
-				points.add(new Point(255, offset));
-			} else if (pendiente > 0) {
-				int cutPoint = (int) (255 - offset / pendiente);
-
-				points.add(new Point(cutPoint, 255));
-				if (cutPoint < 255)
-					points.add(new Point(255, 255));
-			} else {
-				int cutPoint = (int) (255 - offset / pendiente);
-
-				points.add(new Point(cutPoint, 0));
-				if (cutPoint < 255)
-					points.add(new Point(255, 0));
+		int lastPoint = pointsCopy.size() - 1;
+		if (pointsCopy.get(lastPoint).x != UPPER_BOUND) {
+			Pair<Point, Point> res = boundedIntersection(pointsCopy.get(lastPoint - 1), pointsCopy.get(lastPoint));
+			pointsCopy.add(res.getRight());
+			if (res.getRight().getX() < UPPER_BOUND) {
+				pointsCopy.add(new Point(UPPER_BOUND, LOWER_BOUND));
 			}
 		}
+		return pointsCopy;
+	}
+
+	/**
+	 * Calculates the two intersection points of a line in a defined range.
+	 *
+	 * @param pointA first coordinate
+	 * @param pointB second coordinate
+	 * 
+	 * @return pair of intersection points
+	 */
+	private static final Pair<Point, Point> boundedIntersection(final Point pointA, final Point pointB) {// y = ax + b
+		double slope = (pointB.y - pointA.y) / (double) (pointB.x - pointA.x);
+		int offset = (int) (pointA.y - pointA.x * slope);
+		int xLower = (int) ((LOWER_BOUND - offset) / slope);
+		int xUpper = (int) ((UPPER_BOUND - offset) / slope);
+		int yLower = (int) (slope * LOWER_BOUND + offset);
+		int yUpper = (int) (slope * UPPER_BOUND + offset);
+		int[] results = {xLower, xUpper, yLower, yUpper};
+		for (int i = 0; i < results.length; i++) {
+			if (LOWER_BOUND > results[i]) {
+				results[i] = LOWER_BOUND;
+			}
+			if (results[i] > UPPER_BOUND) {
+				results[i] = UPPER_BOUND;
+			}
+		}
+		final Point left;
+		final Point right;
+		if (slope > -0.000001) {
+			left = new Point(results[0], results[2]);
+			right = new Point(results[1], results[3]);
+		} else {			
+			left = new Point(results[1], results[2]);
+			right = new Point(results[0], results[3]);
+		}
+
+		// Looking for slope cutting points
+		return new Pair<>(left, right);
 	}
 
 	static class PointCompare implements Comparator<Point> {
